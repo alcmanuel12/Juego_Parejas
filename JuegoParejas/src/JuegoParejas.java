99132967f88b6c6180e29909bf1b158e1abe0e21
sammytsukino <samy.cabello.g@gmail.com>
2025-01-20 11:39:09 +0100

    20-01

--- JuegoParejas/src/JuegoParejas.java
+++ JuegoParejas/src/JuegoParejas.java
@@ -33,18 +33,18 @@ public class JuegoParejas {
         Scanner scanner = new Scanner(System.in);
         int fila1, columna1, fila2, columna2;
 
-        System.out.println("Introduce la fila y columna del primer número: ");
+        System.out.print("Introduce la fila y columna del primer número: ");
         fila1 = scanner.nextInt();
         columna1 = scanner.nextInt()-1;
 
-        System.out.println("Introduce la fila y columna del segundo número: ");
+        System.out.print("Introduce la fila y columna del segundo número: ");
         fila2 = scanner.nextInt();
         columna2 = scanner.nextInt()-1;
 
         if (tablero[fila1][columna1] == tablero[fila2][columna2]) {
-            System.out.println("¡Correcto! Los números son iguales.");
+            System.out.print("¡Correcto! Los números son iguales.");
         } else {
-            System.out.println("Incorrecto. Los números no son iguales.");
+            System.out.print("Incorrecto. Los números no son iguales.");
         }
         scanner.close();
     }
